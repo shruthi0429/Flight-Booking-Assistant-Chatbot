5cdff58c46055079bf8c927aeeb4ffcb1e3fe132
shruthi0429 <159833546+shruthi0429@users.noreply.github.com>
2025-03-09 01:10:44 -0500

    Add files via upload

---
+++ config_chatbot.py
@@ -0,0 +1,4 @@
+AMADEUS_CLIENT_ID = "your_amadeus_client_id"
+AMADEUS_CLIENT_SECRET = "your_amadeus_client_secret"
+AVIATION_STACK_API_KEY = "your_aviation_stack_key"
+OPENAI_API_KEY = "your_openai_key"
